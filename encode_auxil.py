ee6932e7e4816097ad7d0c9f32c6ff2d6c83e136
subw01f <initials@MacBook.local>
2022-01-31 09:04:30 +1100

    Added String conversion

--- encode_auxil.py
+++ encode_auxil.py
@@ -11,6 +11,68 @@ def toBinary(a):
 		print(i)
 		if i == 32:
 			m.append(str("0100000"))
+		elif i == 33:
+			m.append(str("0100001"))
+		elif i == 34:
+			m.append(str("0100010"))
+		elif i == 35:
+			m.append(str("0100011"))
+		elif i == 36:
+			m.append(str("0100100"))
+		elif i == 37:
+			m.append(str("0100101"))
+		elif i == 38:
+			m.append(str("0100110"))
+		elif i == 39:
+			m.append(str("0100111"))
+		elif i == 40:
+			m.append(str("0101000"))
+		elif i == 41:
+			m.append(str("0101001"))
+		elif i == 42:
+			m.append(str("0101010"))
+		elif i == 43:
+			m.append(str("0101011"))
+		elif i == 44:
+			m.append(str("0101100"))
+		elif i == 45:
+			m.append(str("0101101"))
+		elif i == 46:
+			m.append(str("0101110"))
+		elif i == 47:
+			m.append(str("0101111"))
+		elif i == 48:
+			m.append(str("0110000"))
+		elif i == 49:
+			m.append(str("0110001"))
+		elif i == 50:
+			m.append(str("0110010"))
+		elif i == 51:
+			m.append(str("0110011"))
+		elif i == 52:
+			m.append(str("0110100"))
+		elif i == 53:
+			m.append(str("0110101"))
+		elif i == 54:
+			m.append(str("0110110"))
+		elif i == 55:
+			m.append(str("0110111"))
+		elif i == 56:
+			m.append(str("0111010"))
+		elif i == 57:
+			m.append(str("0111001"))
+		elif i == 58:
+			m.append(str("0111010"))
+		elif i == 59:
+			m.append(str("0111011"))
+		elif i == 60:
+			m.append(str("0111100"))
+		elif i == 61:
+			m.append(str("0111101"))
+		elif i == 62:
+			m.append(str("0111110"))
+		elif i == 63:
+			m.append(str("0111111"))
 		else:
 			m.append(int(bin(i)[2:]))
 	return m
@@ -18,4 +80,6 @@ def toBinary(a):
 
 def split(word):
    	return [char for char in word]
+
+
 
